88a6c513b1f6e04dfaf6613ad4d0e9a502a404fc
NS <157737507+NipunSachintha@users.noreply.github.com>
2025-02-17 17:18:56 +0530

    added user authentication

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+const UserItem = require('../models/Usermodel');
+
+const JWT_SECRET = 'thee_kade';
+
+/*router.post('/login', async (req, res) => {
+    try {
+        const user = await UserItem.find({username:req.body.username,password:req.body.password});
+        res.json(user);
+
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+*/
+
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    try{
+        const user =await UserItem.findOne({ username });
+        if (!user) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+        }
+  
+    
+        //const passwordMatch = await bcrypt.compare(password, user.password);
+        if (password !== user.password) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+        }
+  
+    
+        const token = jwt.sign({ username: user.username, role: user.role }, JWT_SECRET, { expiresIn: '1h' });
+  
+        res.json({"access_token": token, "message": "Login Success","role":user.role});
+    }catch (error) {
+        console.error('Error during login:', error);
+        res.status(500).json({ message: 'Internal server error' });
+      }
+  });
+
+module.exports = router;
